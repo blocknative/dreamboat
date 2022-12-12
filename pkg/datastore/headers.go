6e252cd285657d96549f78e5934735d6d8068c88
Aratz M. Lasa <aratz@blocknative.com>
2022-12-12 16:47:11 +0100

    put back memory check on GetMaxProfitHeader

--- pkg/datastore/headers.go
+++ pkg/datastore/headers.go
@@ -70,6 +70,12 @@ type SlotInfo struct {
 }
 
 func (s *Datastore) GetMaxProfitHeader(ctx context.Context, slot uint64) (structs.HeaderAndTrace, error) {
+	// Check memory
+	p, ok := s.hc.GetMaxProfit(uint64(slot))
+	if ok {
+		return p, nil
+	}
+
 	// Check new
 	p, err := s.getMaxHeader(ctx, slot)
 	if err == nil {
