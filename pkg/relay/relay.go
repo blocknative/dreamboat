82db291336faf0123a8af8c3fee69722f08a8c91
Aratz M. Lasa <aratz@blocknative.com>
2023-01-27 18:30:10 +0100

    fix Timestamp

--- pkg/relay/relay.go
+++ pkg/relay/relay.go
@@ -459,7 +459,7 @@ func (rs *Relay) prepareContents(submitBlockRequest *types.BuilderSubmitBlockReq
 				BlockNumber: s.Payload.Payload.Data.BlockNumber,
 				NumTx:       uint64(len(s.Payload.Payload.Data.Transactions)),
 			},
-			Timestamp: uint64(time.Now().Second()),
+			Timestamp:   uint64(time.Now().UnixMilli() / 1_000),
 			TimestampMs: uint64(time.Now().UnixMilli()),
 		},
 	}
