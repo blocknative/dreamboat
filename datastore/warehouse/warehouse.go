c6268c9eaba4c36023690c12da22fe995a9f4b36
Aratz M. Lasa <aratz@blocknative.com>
2023-04-10 17:11:27 -0600

    reduce fileIdleTime

--- datastore/warehouse/warehouse.go
+++ datastore/warehouse/warehouse.go
@@ -18,7 +18,7 @@ import (
 
 var (
 	fileIdleTime   = structs.DurationPerSlot
-	filesPruneTick = fileIdleTime + (fileIdleTime / 2) // x 1.5
+	fileIdleTime = fileIdleTime + (fileIdleTime / 2) // x 1.5
 	ErrClosed      = errors.New("closed")
 )
 
