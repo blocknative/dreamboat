0908898a7f21219aebc8f72ef07d05a57bde6b7c
Aratz M. Lasa <aratz@blocknative.com>
2023-06-06 11:58:30 -0600

    fix test of /api, /beacon, /auction

--- beacon/manager_test.go
+++ beacon/manager_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/blocknative/dreamboat/beacon/client"
 	"github.com/blocknative/dreamboat/beacon/mocks"
 	"github.com/blocknative/dreamboat/structs"
+	"github.com/flashbots/go-boost-utils/types"
 	"github.com/golang/mock/gomock"
 	"github.com/lthibault/log"
 	"github.com/stretchr/testify/require"
@@ -29,18 +30,31 @@ func TestBeaconClientState(t *testing.T) {
 
 	bm := beacon.NewManager(log.New(), beacon.Config{})
 
-	beaconMock.EXPECT().GetProposerDuties(gomock.Any()).Return(&client.RegisteredProposersResponse{Data: []client.RegisteredProposersResponseData{}}, nil).Times(4)
-	beaconMock.EXPECT().SyncStatus().Return(&client.SyncStatusPayloadData{}, nil).Times(1)
-
-	beaconMock.EXPECT().SubscribeToHeadEvents(gomock.Any(), gomock.Any()).Times(1).DoAndReturn(
-		func(ctx context.Context, events chan client.HeadEvent) {
+	beaconMock.EXPECT().GetProposerDuties(gomock.Any()).Return(&client.RegisteredProposersResponse{Data: []client.RegisteredProposersResponseData{}}, nil).Times(2)
+	beaconMock.EXPECT().KnownValidators(gomock.Any()).Return(client.AllValidatorsResponse{Data: []client.ValidatorResponseEntry{}}, nil).Times(1)
+	beaconMock.EXPECT().SubscribeToPayloadAttributesEvents(gomock.Any()).Times(1).DoAndReturn(
+		func(events chan client.PayloadAttributesEvent) {
 			go func() {
-				events <- client.HeadEvent{Slot: 1}
+				events <- client.PayloadAttributesEvent{Data: client.PayloadAttributesEventData{ProposalSlot: 1, ParentBlockHash: types.Hash{}.String()}}
 			}()
 		},
 	)
-	beaconMock.EXPECT().KnownValidators(gomock.Any()).Return(client.AllValidatorsResponse{Data: []client.ValidatorResponseEntry{}}, nil).Times(1)
-	beaconMock.EXPECT().Genesis().Times(1).Return(structs.GenesisInfo{}, nil)
+
+	stateMock.EXPECT().SetParentBlockHash(types.Hash{}).Times(1)
+	stateMock.EXPECT().ParentBlockHash().Return(types.Hash{}).Times(2)
+	stateMock.EXPECT().SetHeadSlotIfHigher(gomock.Any()).Return(structs.Slot(1), true).Times(1)
+	stateMock.EXPECT().HeadSlot().Return(structs.Slot(1)).Times(1)
+	stateMock.EXPECT().KnownValidators().Return(structs.ValidatorsState{}).Times(1)
+	stateMock.EXPECT().SetDuties(gomock.Any()).Times(1)
+	stateMock.EXPECT().Duties().Return(structs.DutiesState{}).Times(1)
+	stateMock.EXPECT().SetRandao(gomock.Any()).Times(1)
+	stateMock.EXPECT().Randao(gomock.Any(), gomock.Any()).Return(structs.RandaoState{}).Times(1)
+	stateMock.EXPECT().SetWithdrawals(gomock.Any()).Times(1)
+	stateMock.EXPECT().Withdrawals(gomock.Any(), gomock.Any()).Return(structs.WithdrawalsState{}).Times(1)
+	stateMock.EXPECT().SetKnownValidators(gomock.Any()).Times(1)
+	stateMock.EXPECT().KnownValidatorsUpdateTime().Return(time.Now().Add(-time.Hour)).Times(2)
+	stateMock.EXPECT().Fork().Return(structs.ForkState{}).AnyTimes()
+
 	vCache := mocks.NewMockValidatorCache(ctrl)
 
 	var wg sync.WaitGroup
